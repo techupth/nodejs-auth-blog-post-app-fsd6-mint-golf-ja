067a360899f4f31462818198b439fd246a9f1d18
Piyachat Jongtong <wearingandtearing5221@gmail.com>
2024-07-01 16:35:00 +0700

    feat:create auth login and register router

--- server/app.js
+++ server/app.js
@@ -3,28 +3,50 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import postRouter from "./apps/posts.js";
 import { client } from "./utils/db.js";
+import { db } from "./utils/db.js";
+import { posts } from "./data/posts.js";
+import authRouter from "./apps/auth.js";
 
 async function init() {
-  const app = express();
-  const port = 4000;
-
   await client.connect();
 
-  app.use(cors());
-  app.use(bodyParser.json());
-  app.use("/posts", postRouter);
+  try {
+    await db.createCollection("posts");
+    await db.createCollection("users");
+  } catch (error) {
+    console.log(error.message);
+  }
 
-  app.get("/", (req, res) => {
-    res.send("Hello World!");
-  });
+  const collection = db.collection("posts");
 
-  app.get("*", (req, res) => {
-    res.status(404).send("Not found");
-  });
-
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
+  await collection.insertMany(
+    posts.map((post) => {
+      return {
+        ...post,
+        created_at: new Date(post.created_at),
+      };
+    })
+  );
 }
 
 init();
+
+const app = express();
+const port = 5003;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use("/posts", postRouter);
+app.use("/auth", authRouter);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("*", (req, res) => {
+  res.status(404).send("Not found");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- server/apps/auth.js
+++ server/apps/auth.js
@@ -1,11 +1,56 @@
 import { Router } from "express";
+import { db } from "../utils/db.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const authRouter = Router();
 
 // 🐨 Todo: Exercise #1
 // ให้สร้าง API เพื่อเอาไว้ Register ตัว User แล้วเก็บข้อมูลไว้ใน Database ตามตารางที่ออกแบบไว้
+authRouter.post("/register", async (req, res) => {
+  const collection = db.collection("users");
+  const userData = { ...req.body };
+  const salt = await bcrypt.genSalt(10);
+  userData.password = bcrypt.hash(userData.password, salt);
+  try {
+    await collection.insertOne(userData);
+    return res.status(201).json({
+      message: "User has been created successfully",
+    });
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+});
 
 // 🐨 Todo: Exercise #3
 // ให้สร้าง API เพื่อเอาไว้ Login ตัว User ตามตารางที่ออกแบบไว้
+authRouter.post("/login", async (req, res) => {
+  const user = await db
+    .collection("users")
+    .findOne({ username: req.body.username });
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found",
+    });
+  }
+  const isValidPassword = bcrypt.compare(req.body.password, user.password);
+  if (!isValidPassword) {
+    return res.status(401).json({
+      message: "Invalid username or password",
+    });
+  }
+
+  const token = jwt.sign(
+    {
+      id: user._id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+    },
+    process.env.SECRET_KEY,
+    { expiresIn: 900000 }
+  );
+
+  return res.json({ message: "Login Successfully", token });
+});
 
 export default authRouter;
