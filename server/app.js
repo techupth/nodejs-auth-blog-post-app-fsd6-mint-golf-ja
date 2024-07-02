a7a0456033399312c39a4fb53b5e07be8afcb572
Piyachat Jongtong <wearingandtearing5221@gmail.com>
2024-07-02 16:31:41 +0700

    fix: create auth func both front and back

--- server/app.js
+++ server/app.js
@@ -2,34 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import postRouter from "./apps/posts.js";
-import { client } from "./utils/db.js";
-import { db } from "./utils/db.js";
-import { posts } from "./data/posts.js";
 import authRouter from "./apps/auth.js";
-
-async function init() {
-  await client.connect();
-
-  try {
-    await db.createCollection("posts");
-    await db.createCollection("users");
-  } catch (error) {
-    console.log(error.message);
-  }
-
-  const collection = db.collection("posts");
-
-  await collection.insertMany(
-    posts.map((post) => {
-      return {
-        ...post,
-        created_at: new Date(post.created_at),
-      };
-    })
-  );
-}
-
-init();
 
 const app = express();
 const port = 5003;
